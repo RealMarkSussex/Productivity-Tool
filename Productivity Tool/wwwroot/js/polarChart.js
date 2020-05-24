c339493862e5a72785d878e61aee9fd996f0455a
RealMarkSussex <marksussex6@gmail.com>
2020-05-24 22:00:07 +0100

    extracted js into separate class

---
+++ Productivity Tool/wwwroot/js/polarChart.js
@@ -0,0 +1,21 @@
+﻿var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    data: {
+        datasets: [{
+            data: [10, 20, 30],
+            backgroundColor: ['Red', 'Yellow', 'Blue'],
+            hoverBackgroundColor: ['Red', 'Yellow', 'Blue']
+        }],
+
+        // These labels appear in the legend and in the tooltips when hovering different arcs
+        labels: [
+            'Red',
+            'Yellow',
+            'Blue'
+        ]
+    },
+    type: 'polarArea',
+    options: {
+        borderAlign: 'centre'
+    }
+});
