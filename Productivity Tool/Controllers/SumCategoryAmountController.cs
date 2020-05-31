acb5b506267741d37166ca233607f72d727bed1b
RealMarkSussex <marksussex6@gmail.com>
2020-05-31 22:42:37 +0100

    sum category amount implemented

---
+++ Productivity Tool/Controllers/SumCategoryAmountController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLogic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Productivity_Tool.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SumCategoryAmountController : ControllerBase
+    {
+        private readonly SpendItemService _spendItemService;
+
+        public SumCategoryAmountController(SpendItemService spendItemService)
+        {
+            _spendItemService = spendItemService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_spendItemService.SumCategorySpendAmounts(User.Identity.Name));
+        }
+    }
+}
--- Productivity Tool/wwwroot/js/polarChart.js
+++ Productivity Tool/wwwroot/js/polarChart.js
@@ -1,21 +1,75 @@
-﻿var ctx = document.getElementById('myChart').getContext('2d');
-var chart = new Chart(ctx, {
-    data: {
-        datasets: [{
-            data: [10, 20, 30],
-            backgroundColor: ['Red', 'Yellow', 'Blue'],
-            hoverBackgroundColor: ['Red', 'Yellow', 'Blue']
-        }],
+﻿let sumCategoryAmounts = [];
 
-        // These labels appear in the legend and in the tooltips when hovering different arcs
-        labels: [
-            'Red',
-            'Yellow',
-            'Blue'
-        ]
-    },
-    type: 'polarArea',
-    options: {
-        borderAlign: 'centre'
+fetch("https://localhost:44385/api/SumCategoryAmount")
+    .then(res => res.json())
+    .then(result => {
+        sumCategoryAmounts = result;
+        var category = sumCategoryAmounts.map(item => GetCategory(item.category));
+        var totalAmount = sumCategoryAmounts.map(item => Math.round(item.totalAmount));
+
+        var ctx = document.getElementById('myChart').getContext('2d');
+
+        var chart = new Chart(ctx, {
+            data: {
+                datasets: [{
+                    data: totalAmount,
+                    backgroundColor: ['Red', 'Yellow', 'Blue', 'Orange', 'Green', 'Black'],
+                    hoverBackgroundColor: ['Red', 'Yellow', 'Blue', 'Orange', 'Green', 'Black']
+                }],
+
+                // These labels appear in the legend and in the tooltips when hovering different arcs
+                labels: category
+            },
+            type: 'pie',
+            options: {
+                borderAlign: 'centre'
+            }
+        });
+    });
+
+//sumCategoryAmounts = [
+//    { "totalAmount": 4.56, "category": 0 }, { "totalAmount": 17.89, "category": 1 }, { "totalAmount": 8.32, "category": 2 },
+//    { "totalAmount": 2.87, "category": 3 }, { "totalAmount": 6.99, "category": 4 },
+//    { "totalAmount": 2.99, "category": 5 }
+//];
+
+//var category = sumCategoryAmounts.map(item => GetCategory(item.category));
+//var totalAmount = sumCategoryAmounts.map(item => Math.round(item.totalAmount));
+
+//var ctx = document.getElementById('myChart').getContext('2d');
+
+//var chart = new Chart(ctx, {
+//    data: {
+//        datasets: [{
+//            data: totalAmount,
+//            backgroundColor: ['Red', 'Yellow', 'Blue', 'Orange', 'Green', 'Black'],
+//            hoverBackgroundColor: ['Red', 'Yellow', 'Blue', 'Orange', 'Green', 'Black']
+//        }],
+
+//        // These labels appear in the legend and in the tooltips when hovering different arcs
+//        labels: category
+//    },
+//    type: 'pie',
+//    options: {
+//        borderAlign: 'centre'
+//    }
+//});
+
+function GetCategory(category) {
+    switch(category) {
+        case 0:
+            return "Gaming";
+        case 1:
+            return "Snacks";
+        case 2:
+            return "Technology";
+        case 3:
+            return "Subscriptions";
+        case 4:
+            return "Rent";
+        case 5:
+            return "Travel";
+        default:
+            throw `Category:${category} is not implemented.`;
     }
-});
+}
