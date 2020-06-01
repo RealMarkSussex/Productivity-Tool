602c15bab5b60beb64236c114236d839cf0ea759
RealMarkSussex <marksussex6@gmail.com>
2020-06-01 22:01:06 +0100

    improved javascript formatting

--- Productivity Tool/wwwroot/js/polarChart.js
+++ Productivity Tool/wwwroot/js/polarChart.js
@@ -1,59 +1,29 @@
-﻿let sumCategoryAmounts = [];
-
-fetch("https://localhost:44385/api/SumCategoryAmount")
+﻿fetch("https://localhost:44385/api/SumCategoryAmount")
     .then(res => res.json())
     .then(result => {
-        sumCategoryAmounts = result;
+        let sumCategoryAmounts = result;
         var category = sumCategoryAmounts.map(item => GetCategory(item.category));
-        var totalAmount = sumCategoryAmounts.map(item => Math.round(item.totalAmount));
+        var totalAmount = sumCategoryAmounts.map(item => Math.round(item.totalAmount).toString());
 
-        var ctx = document.getElementById('myChart').getContext('2d');
+        var ctx = document.getElementById("myChart").getContext("2d");
 
-        var chart = new Chart(ctx, {
+        var chart = new window.Chart(ctx, {
             data: {
                 datasets: [{
                     data: totalAmount,
-                    backgroundColor: ['Red', 'Yellow', 'Blue', 'Orange', 'Green', 'Black'],
-                    hoverBackgroundColor: ['Red', 'Yellow', 'Blue', 'Orange', 'Green', 'Black']
+                    backgroundColor: ["Red", "Yellow", "Blue", "Orange", "Green", "Black"],
+                    hoverBackgroundColor: ["Red", "Yellow", "Blue", "Orange", "Green", "Black"]
                 }],
 
                 // These labels appear in the legend and in the tooltips when hovering different arcs
                 labels: category
             },
-            type: 'pie',
+            type: "pie",
             options: {
-                borderAlign: 'centre'
+                borderAlign: "centre"
             }
         });
     });
-
-//sumCategoryAmounts = [
-//    { "totalAmount": 4.56, "category": 0 }, { "totalAmount": 17.89, "category": 1 }, { "totalAmount": 8.32, "category": 2 },
-//    { "totalAmount": 2.87, "category": 3 }, { "totalAmount": 6.99, "category": 4 },
-//    { "totalAmount": 2.99, "category": 5 }
-//];
-
-//var category = sumCategoryAmounts.map(item => GetCategory(item.category));
-//var totalAmount = sumCategoryAmounts.map(item => Math.round(item.totalAmount));
-
-//var ctx = document.getElementById('myChart').getContext('2d');
-
-//var chart = new Chart(ctx, {
-//    data: {
-//        datasets: [{
-//            data: totalAmount,
-//            backgroundColor: ['Red', 'Yellow', 'Blue', 'Orange', 'Green', 'Black'],
-//            hoverBackgroundColor: ['Red', 'Yellow', 'Blue', 'Orange', 'Green', 'Black']
-//        }],
-
-//        // These labels appear in the legend and in the tooltips when hovering different arcs
-//        labels: category
-//    },
-//    type: 'pie',
-//    options: {
-//        borderAlign: 'centre'
-//    }
-//});
 
 function GetCategory(category) {
     switch(category) {
