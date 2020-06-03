21bf869c6738c6814cc324ce3ab0ed4da938370b
RealMarkSussex <marksussex6@gmail.com>
2020-06-03 10:52:19 +0100

    onyl allowed access to tracker if signed in

--- Productivity Tool/Controllers/HomeController.cs
+++ Productivity Tool/Controllers/HomeController.cs
@@ -27,11 +27,6 @@ namespace Productivity_Tool.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
